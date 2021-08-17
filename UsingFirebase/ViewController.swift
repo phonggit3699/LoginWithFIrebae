97c98642e1db8bc84c81739f29431690b38590d6
phonggit3699 <phamphong3699@gmail.com>
2021-08-17 08:10:30 +0700

    Initial Commit

---
+++ UsingFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingFirebase
+//
+//  Created by PHONG on 17/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
